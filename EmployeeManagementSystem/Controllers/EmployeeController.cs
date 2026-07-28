90a90c4fb0c519aa2e2f267e133fc442ad34addb
Sarthak Sinha <167212360+sarthaksinha2003@users.noreply.github.com>
2026-07-28 18:04:26 +0530

    Add employee list and details functionality

---
+++ EmployeeManagementSystem/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using EmployeeManagementSystem.Models;
+using EmployeeManagementSystem.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly EmployeeService employeeService;
+        public EmployeeController(EmployeeService employeeService)
+        {
+            this.employeeService = employeeService;
+        }
+
+        public IActionResult List()
+        {
+            List<Employee> employees = this.employeeService.GetEmployees();
+            return View(employees);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Employee? employee = employeeService.GetEmployeeById(id);
+            return View(employee);
+        }
+    }
+}
---
+++ EmployeeManagementSystem/Services/EmployeeService.cs
@@ -0,0 +1,46 @@
+﻿using EmployeeManagementSystem.Models;
+
+namespace EmployeeManagementSystem.Services
+{
+    public class EmployeeService
+    {
+        public List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new();
+            Employee ep1 = new();
+            ep1.Id = 1;
+            ep1.Name = "Sarthak";
+            ep1.Department = "IT";
+            ep1.Salary = 50000;
+            employees.Add(ep1);
+
+            Employee ep2 = new();
+            ep2.Id = 2;
+            ep2.Name = "Rahul";
+            ep2.Department = "IT";
+            ep2.Salary = 45000;
+            employees.Add(ep2);
+
+            Employee ep3 = new();
+            ep3.Id = 3;
+            ep3.Name = "PinkMan";
+            ep3.Department = "Sales";
+            ep3.Salary = 30000;
+            employees.Add(ep3);
+
+            return employees;
+        }
+        public Employee? GetEmployeeById(int id)
+        {
+            List<Employee> employees = GetEmployees();
+            foreach(Employee emp in employees)
+            {
+                if(emp.Id == id)
+                {
+                    return emp;
+                }
+            }
+            return null;
+        }
+    }
+}
